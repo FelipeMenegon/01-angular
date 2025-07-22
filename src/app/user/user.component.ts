318529d679872c64848edb7be428e72af06f6ea3
FelipeM <126184555+FelipeMenegon@users.noreply.github.com>
2025-07-22 12:53:37 -0300

    0.0.4

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, input, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,16 +7,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  //@Input({required: true}) avatar!: string;
+  //@Input({required: true}) name!: string;
 
-  get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar
+  avatar = input<string>();
+  name = input<string>();
+
+  get imagePath () {
+    return "assets/users/" + this.avatar;
   }
 
-
-  onSelectUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
-
+  onSelectUser(){}
 }
