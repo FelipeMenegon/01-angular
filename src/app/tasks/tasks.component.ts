d2d9de8b079a0a80b9f5e8a692eabc52bb684285
FelipeM <126184555+FelipeMenegon@users.noreply.github.com>
2025-07-28 14:11:26 -0300

    0.0.5

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,12 @@
+import { DUMMY_USERS } from './../dummy-users';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input() name?: string;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,40 @@
-import { Component, input, Input} from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  //@Input({required: true}) avatar!: string;
-  //@Input({required: true}) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
+  @Output() select = new EventEmitter<string>();
+  //select = output<string>();
 
-  avatar = input<string>();
-  name = input<string>();
+  //avatar = input.required<string>();
+  //name = input.required<string>();
 
-  get imagePath () {
-    return "assets/users/" + this.avatar;
+  //  imagePath = computed(()=>{
+  //    return "assets/users/" + this.avatar;
+  //  });
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
   }
 
-  onSelectUser(){}
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 }
