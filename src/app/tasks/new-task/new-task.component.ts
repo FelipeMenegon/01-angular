becac5ec0faa99580c195c4f01649086d990dda1
FelipeM <126184555+FelipeMenegon@users.noreply.github.com>
2025-08-12 12:57:54 -0300

    0.0.9

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NewTaskData } from './new-task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -9,20 +10,25 @@ import { NewTaskData } from './new-task.model';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
   insertTitle = '';
   insertSummary = '';
   insertDate = '';
+  private tasksService = inject(TasksService);
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
   onSubmit() {
-    this.add.emit({
-      title: this.insertTitle,
-      summary: this.insertSummary,
-      date: this.insertDate,
-    });
+    this.tasksService.addTask(
+      {
+        title: this.insertTitle,
+        summary: this.insertSummary,
+        date: this.insertDate,
+      },
+      this.userId
+    );
+    this.close.emit();
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -15,27 +15,18 @@ export class TasksComponent {
   @Input({ required: true }) name!: string;
   @Input({ required: true }) userId!: string;
   isAddingTask = false;
-  
-  constructor(private tasksService: TasksService) {}
 
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
-  }
-
-  onCompleteTask(id: string) {
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
-    this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
