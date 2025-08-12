9c0ce9bc44417702c671a76a18ac6715c846a167
FelipeM <126184555+FelipeMenegon@users.noreply.github.com>
2025-08-12 10:25:27 -0300

    0.0.8
    Injectable Dependencies

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,28 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from './new-task.model';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>;
+  @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  insertTitle = '';
+  insertSummary = '';
+  insertDate = '';
 
-  onCancel(){
+  onCancel() {
     this.cancel.emit();
   }
-
+  onSubmit() {
+    this.add.emit({
+      title: this.insertTitle,
+      summary: this.insertSummary,
+      date: this.insertDate,
+    });
+  }
 }
